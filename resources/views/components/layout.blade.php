a92b000720df10da0ab95b7ea51abd00106a9eb2
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2026-02-19 15:05:45 +0300

    Blade and Tailwind Techniques for Your Laravel Views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap"
+        rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white">
 
+<body class="bg-black text-white font-hanken-grotesk">
 <div class="px-10">
     <nav class="flex justify-between items-center py-4 border-b border-white/10">
         <div>
@@ -28,11 +33,10 @@
         <div>
             <a href="">Post a Job</a>
         </div>
-
     </nav>
 
     <main class="mt-10 max-w-[986px] mx-auto">
-        {{$slot}}
+        {{ $slot }}
     </main>
 </div>
 
