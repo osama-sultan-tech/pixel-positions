9c11e108aed9e4ba4f82437ffbd5a9007425bc9f
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2026-02-21 07:47:24 +0300

    Jobs, Tags, TDD, Oh My!

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,4 +1,4 @@
-@props(['size' => 'base'])
+@props(['tag','size' => 'base'])
 
 
 @php
@@ -13,4 +13,5 @@
     }
 @endphp
 
-<a href="#" class="{{ $classes }}">{{ $slot }}</a>
+{{--<a href="#" class="{{ $classes }}">{{ $slot }}</a>--}}
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
